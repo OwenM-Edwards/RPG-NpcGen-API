11c09d088a7a95c39a6c2d0e44f59b939221bbd8
Quibbly <owenhedwards@gmail.com>
2019-11-14 16:56:05 +0000

    Changed name randomizer to knex query

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const knex = require('knex')
 
+
 const database = knex({
    client:'pg',
    connection: {
@@ -12,10 +13,13 @@ const database = knex({
       database:'rpgnpcgen'
    }
 })
+ 
+
 
 
 const app =  express();
 app.use(bodyParser.json());
+app.use(cors())
 
 // TEMP DATABASES
 const raceDatabase ={
@@ -46,44 +50,26 @@ constructChar=(race, res)=>{
          return res.status(200).json(item);
       }
    })
-   
+  
    // let charName = raceDatabase.race[Math.floor(Math.random() * raceDatabase.human.length)];
    // return charName;
 }
 
-pickRandomProperty = (obj) => {
-   var result;
-   var count = 0;
-   for (var prop in obj)
-       if (Math.random() < 1/++count)
-          result = prop;
-   return result;
-}
-
-
-generateRanID = (idMax) => {
-   return Math.random() * idMax;    
-}
 app.get('/', (req, res)=> {
    res.send('this is working');
 }) 
 
-app.get('/genchar', (req, res)=>{
+app.post('/genchar', (req, res)=>{
    // res.send(constructChar(req.body.race, res));
    // database.select('*').from('nameshuman').then(data => {
    //    res.json(data);
    // });
    let gender = req.body.gender;
    let race = req.body.race;
-   let id = Math.floor(generateRanID(10));
-   if(id > 10){
-      id = 10;
-   } else if(id < 1){
-      id = 1;
-   }
-   database(race).where({
-      id: id,
-   }).select(gender).then(data=>{
+   database(race).select(gender).orderByRaw('RANDOM() LIMIT 1')
+   
+   
+   .then(data=>{
       res.json(data);
    })
 
@@ -93,9 +79,3 @@ app.get('/genchar', (req, res)=>{
 app.listen(3000, ()=> {
    console.log("App running port 3000")
 })
-
-
-
-
-// --> this is working
-// getChar --> GET = Race/role/system
