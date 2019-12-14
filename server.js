1f0c98caeba2fb672405ccf582b2467e31b2ad17
Quibbly <owenhedwards@gmail.com>
2019-12-14 13:57:26 +0000

    Created database on Heroku

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ cloudinary.config({
 const db = knex({
    client:'pg',
    connection: {
-      connectionString:process.env.DATABASE_URL,
+      connectionString: process.env.DATABASE_URL,
       ssl : true,
    }
 })
