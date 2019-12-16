103e87686e5397ac8be48711ac2bda207a7f356d
Quibbly <owenhedwards@gmail.com>
2019-12-16 21:09:22 +0000

    Console log heroku test

--- controllers/cloudnotification.js
+++ controllers/cloudnotification.js
@@ -1,10 +1,3 @@
 const handleCloudNotification = (req, res, db,) => {
-   db('mod_nameshuman')
-         .insert({'name': req.body[0]})
-         .then(data=> {
-            res.status(200).json('success')
-         })
-         .catch(error=>{
-            res.status(800).json('duplicate last name')
-         })
+   console.log(req)
 }
