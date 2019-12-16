629dfc2750569bfd73e352faf44ad6f4b5602211
Quibbly <owenhedwards@gmail.com>
2019-12-16 21:15:41 +0000

    test

--- controllers/cloudnotification.js
+++ controllers/cloudnotification.js
@@ -1,3 +1,10 @@
 const handleCloudNotification = (req, res, db,) => {
-   console.log(req)
+   db('mod_nameshuman')
+         .insert({'name': req.body[0]})
+         .then(data=> {
+            res.status(200).json('success')
+         })
+         .catch(error=>{
+            res.status(800).json('duplicate last name')
+         })
 }
