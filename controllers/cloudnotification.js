df3625f9d7befe8f11df7f15e043d2a4455f7bca
Quibbly <owenhedwards@gmail.com>
2019-12-21 17:47:22 +0000

    test database change

--- controllers/cloudnotification.js
+++ controllers/cloudnotification.js
@@ -1,11 +1,11 @@
 const handleCloudNotification = (req, res, db,) => {
    let saveToDb = req.body
    db('test_table')
-         .insert({'test': 'hey'})
-         .then(data=> {
-            res.status(200).json('success')
-         })
-         .catch(error=>{
-            res.status(800).json('duplicate last name')
-         })
+      .insert({test: 'hey'})
+      .then(data=> {
+         res.status(200).json('success')
+      })
+      .catch(error=>{
+         res.status(800).json('duplicate last name')
+      })
 }
