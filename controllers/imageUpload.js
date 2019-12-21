404e11f5cd6e32ab4f9dda2ea00ad35751f27eb7
Quibbly <owenhedwards@gmail.com>
2019-12-21 21:55:32 +0000

    testing

--- controllers/imageUpload.js
+++ controllers/imageUpload.js
@@ -1,7 +1,18 @@
 //POSTING IMAGES
 const handleCharImage = (req, res, db, cloudinary) => {
-
-   saveImageToDatabase(req, result.url, result.public_id, db)
+   cloudinary.uploader.upload(req.body.image[0].src.base64, 
+      { moderation: 'manual' },
+      { notification_url: "https://safe-dawn-37731.herokuapp.com/cloudnotification"  },
+      function(error, result) {
+         // saveImageToDatabase(req, result.url, result.public_id, db)
+         console.log(result)
+      })
+      .then(data=>{
+         res.status(200).json(data)
+      })
+      .catch( 
+         res.status(400).json('Error adding image')
+      )
 };
 saveImageToDatabase = (req,url, id,db) => {
    if(req.body.gender == 'male'){
@@ -11,17 +22,9 @@ saveImageToDatabase = (req,url, id,db) => {
    }
    let race = req.body.race;
    let role = req.body.role;
-   db('test_table')
-      .insert({'test': role})
-      .then(data=> {
-         db('img'+race).insert({'url':url, 'role':role, 'key':id, 'gender':gender})
-         .then(data=>{
-         })
-      })
-      .catch(error=>{
-         res.status(800).json('duplicate last name')
-      })
-   
+   db('img'+race).insert({'url':url, 'role':role, 'key':id, 'gender':gender})
+   .then(data=>{
+   })
 }
 
 module.exports = {
