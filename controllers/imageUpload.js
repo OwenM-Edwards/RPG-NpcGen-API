ec42c8d63f9884521676e1d6b371e60d2fda73b6
Quibbly <owenhedwards@gmail.com>
2019-12-22 16:11:25 +0000

    test

--- controllers/imageUpload.js
+++ controllers/imageUpload.js
@@ -1,18 +1,13 @@
 //POSTING IMAGES
 const handleCharImage = (req, res, db, cloudinary) => {
-   cloudinary.uploader.upload(req.body.image[0].src.base64, 
-      { moderation: 'manual' },
-      { notification_url: "https://safe-dawn-37731.herokuapp.com/cloudnotification"  },
-      function(error, result) {
-         // saveImageToDatabase(req, result.url, result.public_id, db)
-         console.log(result)
+   db('test_table')
+      .insert({test: 'hey'})
+      .then(data=> {
+         res.status(200).json('success')
       })
-      .then(data=>{
-         res.status(200).json(data)
+      .catch(error=>{
+         res.status(800).json('duplicate last name')
       })
-      .catch( 
-         res.status(400).json('Error adding image')
-      )
 };
 saveImageToDatabase = (req,url, id,db) => {
    if(req.body.gender == 'male'){
