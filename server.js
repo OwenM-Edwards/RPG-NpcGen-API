a6660c481e4552576622740ffa44ab9856e351bd
Quibbly <owenhedwards@gmail.com>
2019-12-27 12:37:54 +0000

    testing

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.post('/genchar', (req, res)=>{ genchar.handleGenChar(req,res,db)});
 //REPLY FROM CLOUDINARY MODERATION
 app.post('/cloudnotification', (req, res)=>{ 
    db('test_table')
-      .insert({test: req.body})
+      .insert({test: "hello"})
       .then(data=> {
          res.status(200).json('success')
       })
