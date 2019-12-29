751b825285ad8685016608ed38860889077d2567
Quibbly <owenhedwards@gmail.com>
2019-12-29 17:10:24 +0000

    testing

--- controllers/imageUpload.js
+++ controllers/imageUpload.js
@@ -2,16 +2,17 @@
 const handleCharImage = (req, res, db, cloudinary) => {
    cloudinary.uploader.upload(req.body.image[0].src.base64, 
       { moderation: 'manual' },
+      {tags: 'basic_sample'},
       function(error, result) {
          if (error) {
             // handle error
          } else {
-            saveImageToDatabase(req, req.body.url, req.body.id, db)
+            saveImageToDatabase(req, result, db)
             console.log(result);
          }
       });
 };
-saveImageToDatabase = (req,url, id,db) => {
+saveImageToDatabase = (req,result,db) => {
    if(req.body.gender == 'male'){
       var charGender = true
    } else if(req.body.gender == 'female'){
@@ -19,6 +20,8 @@ saveImageToDatabase = (req,url, id,db) => {
    }
    let race = req.body.race;
    let role = req.body.role;
+   let url = result.body.url
+   let id = result.body.public_id
    db('img'+race).insert({'url':url, 'role':role, 'key':id, 'gender':charGender})
    .then(data=>{
    })
