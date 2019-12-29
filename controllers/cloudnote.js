570f2b85c88af5f770663eb31c1ba169a55791ff
Quibbly <owenhedwards@gmail.com>
2019-12-29 18:47:17 +0000

    test

--- controllers/cloudnote.js
+++ controllers/cloudnote.js
@@ -2,16 +2,17 @@ const handleCloudNote = (req, res, db,) => {
    if(req.body.moderation_status === 'approved') {
       console.log(req.body)
 
-      // db('img' + req.body.)
-      //    .insert({'moderation':true})
-      //    .where({'key':req.body.public_id})
-      //    .then(data=>{
-      //       res.status(200).json({ success: true})
-      //    }) 
-      //    .catch(error=>{
-      //       console.log(error)
-      //       res.status(405).json({ success: false})
-      //    })
+      db({a:'imghuman', b:'imgorc'})
+         .insert({'moderation':true})
+         .where(a.key, req.body.public_id)
+         .where(b.key, req.body.public_id)
+         .then(data=>{
+            res.status(200).json({ success: true})
+         }) 
+         .catch(error=>{
+            console.log(error)
+            res.status(405).json({ success: false})
+         })
    } else {
       console.log('no');
    }
