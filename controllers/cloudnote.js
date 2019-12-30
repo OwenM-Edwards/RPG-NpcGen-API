4f1b52648fb28d0e9328344715d3581f8ccdb2ac
Quibbly <owenhedwards@gmail.com>
2019-12-30 21:36:18 +0000

    Fixed Cloudinary API moderation notification

--- controllers/cloudnote.js
+++ controllers/cloudnote.js
@@ -2,10 +2,9 @@ const handleCloudNote = (req, res, db,) => {
    if(req.body.moderation_status === 'approved') {
       console.log(req.body)
 
-      db({a:'imghuman', b:'imgorc'})
+      db('imghuman').fullOuterJoin('imgorc', 'key')
          .insert({'moderation':true})
-         .where(a.key, req.body.public_id)
-         .where(b.key, req.body.public_id)
+         .where({'key':req.body.public_id})
          .then(data=>{
             res.status(200).json({ success: true})
          }) 
