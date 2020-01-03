1d53673eda8cdc81f93f725279f65ad32f631d46
Quibbly <owenhedwards@gmail.com>
2020-01-03 11:55:06 +0000

    testing

--- controllers/cloudnote.js
+++ controllers/cloudnote.js
@@ -23,8 +23,8 @@ const handleCloudNote = (req, res, db,) => {
 
 searchHumanTable = (imgKey, db) =>{
    db('imghuman')
-      .insert({'moderation':true})
       .where({'key':imgKey})
+      .update({'moderation':true})
    .then(data=>{
       console.log(data)
    })
@@ -34,8 +34,8 @@ searchHumanTable = (imgKey, db) =>{
 }
 searchOrcTable = (imgKey, db) =>{
    db('imgorc')
-      .insert({'moderation':true})
       .where({'key':imgKey})
+      .update({'moderation':true})
    .then(data=>{
       console.log(data)
    })
