6a5f6d70200fc10fd2473ba84a56e210640ebc37
Quibbly <owenhedwards@gmail.com>
2020-01-06 11:30:00 +0000

    testing

--- controllers/genchar.js
+++ controllers/genchar.js
@@ -36,7 +36,7 @@ const handleGenChar = (req, res, db,) => {
       })
    }
    generateCharFirstName(db,race,gender, (data)=>{
-      returnedChar[0] = data.name;
+      returnedChar[0] = data[0].name;
       console.log(returnedChar);
    });
 
