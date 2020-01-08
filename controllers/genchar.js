a6eb948e20d6062e5019954bfd30ad8447da59ea
Quibbly <owenhedwards@gmail.com>
2020-01-08 16:03:48 +0000

    testing

--- controllers/genchar.js
+++ controllers/genchar.js
@@ -43,13 +43,15 @@ const handleGenChar = (req, res, db,) => {
       //GET CHAR RACE MAX AGE
       .select('c.maxage')
       .where('c.race', race)
+      //GET CHAR LAST NAME
+      .select('d.lastname')
 
       //GET INTRIGUE
       .select('e.intrigue')
-      .orderByRaw('RANDOM() LIMIT 1')
+
       //GET THREE ROLEPLAY Q'S
       .select('f.roleplay')
-      .orderByRaw('RANDOM() LIMIT 3')
+
    .then(data=>{
       res.status(200).json(data)
    })
